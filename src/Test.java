d74c43bb256050e03cc9e334882debe26b53567b
admin <jacek.krk@gail.com>
2022-05-27 21:43:35 +0200

    Zadanie

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,37 @@
+// wypisac liczby od 1 do 100 podzielne przez 3
+
+//
+
 public class Test {
+
+    static int dviade = 5;
+    static int resoult, narrow;
+    static int rangeNumber = 1324;
+    static int displayNumberInLinie = 10;
+    static int liczbaWierszy = 10;
+    static int[] zadanie = {1 , 3 ,5 } ;
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= rangeNumber; i++) {
+
+            resoult = i % dviade;
+            if (resoult == 0 && i != 0) {
+                System.out.print(i + " ");
+                narrow++;
+                if (narrow == liczbaWierszy) {
+                    narrow = 0;
+                    System.out.println();
+
+
+                }
+            }
+
+        }
+
+  int lenght = zadanie.length;
+        System.out.println(lenght);}
 }
+
+
+
+
